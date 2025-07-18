2c46e3058b9078a5f8ff2632ef4ff2c0af9cbc77
Santiago Soler <santisoler@fastmail.com>
2025-07-18 12:16:25 -0700

    Work on data misfit class

--- src/inversion_ideas/data_misfit.py
+++ src/inversion_ideas/data_misfit.py
@@ -1,7 +1,8 @@
 """
 Class to represent a data misfit term.
 """
-import numpy as np
+from scipy.sparse import diags_array
+
 from .objective_function import Objective
 
 
@@ -10,20 +11,31 @@ class DataMisfit(Objective):
     L2 data misfit term.
     """
 
-    def __init__(self, data, weights, simulation):
+    def __init__(self, data, uncertainty, simulation):
         self.data = data
-        self.weights = weights
+        self.uncertainty = uncertainty
         self.simulation = simulation
 
     def __call__(self, model) -> float:  # noqa: D102
+        weights_sq = diags_array(self.uncertainty)  # W.T @ W
         residual = self.residual(model)
-        return np.sum(residual.T @ residual)
+        return residual.T @ weights_sq @ residual
 
-    def gradient(self):
+    def gradient(self, model):
         """
-        Number of model parameters.
+        Gradient vector.
         """
-        return self.simulation.n_params
+        weights_sq = diags_array(self.uncertainty)  # W.T @ W
+        jac = self.simulation.jacobian(model)
+        return -2 * jac.T @ weights_sq @ self.residual
+
+    def hessian(self, model):
+        """
+        Hessian matrix.
+        """
+        weights_sq = diags_array(self.uncertainty)  # W.T @ W
+        jac = self.simulation.jacobian(model)
+        return 2 * jac.T @ weights_sq @ jac
 
     @property
     def n_params(self):
@@ -32,8 +44,36 @@ class DataMisfit(Objective):
         """
         return self.simulation.n_params
 
+    @property
+    def n_data(self):
+        """
+        Number of data values.
+        """
+        return self.data.size
+
     def residual(self, model):
+        r"""
+        Residual vector.
+
+        Parameters
+        ----------
+        model : (n_params) array
+            Array with model values.
+
+        Returns
+        -------
+        (n_data) array
+            Array with residual vector.
+
+        Notes
+        -----
+        Residual vector defined as:
+
+        .. math::
+
+            \mathbf{r} = \mathbf{d} - \mathcal{F}(\mathbf{m})
+
+        where :math:`\mathbf{d}` is the vector with observed data, :math:`\mathcal{F}`
+        is the forward model, and :math:`\mathbf{m}` is the model vector.
         """
-        Residual.
-        """
-        return (self.data - self.simulation(model)) / self.weights
+        return self.data - self.simulation(model)

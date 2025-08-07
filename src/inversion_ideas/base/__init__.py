b9d9c79e63a0d653ceb1c8941dbc35f1b9e2a9e7
Santiago Soler <santisoler@fastmail.com>
2025-08-07 11:23:58 -0700

    Add directives to Inversion

--- src/inversion_ideas/base/__init__.py
+++ src/inversion_ideas/base/__init__.py
@@ -2,6 +2,7 @@
 Base classes of the inversion framework.
 """
 from .conditions import Condition
+from .directive import Directive
 from .minimizer import Minimizer
 from .objective_function import Combo, Objective, Scaled
 from .simulation import Simulation
@@ -9,6 +10,7 @@ from .simulation import Simulation
 __all__ = [
     "Combo",
     "Condition",
+    "Directive",
     "Minimizer",
     "Objective",
     "Scaled",

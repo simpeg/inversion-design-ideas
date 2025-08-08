1d8e39e571545b9bebe6e3482438e455d1adc737
Santiago Soler <santisoler@fastmail.com>
2025-08-08 00:18:15 +0000

    Move log constructor to a classmethod (#10)

--- src/inversion_ideas/constructors.py
+++ src/inversion_ideas/constructors.py
@@ -2,31 +2,9 @@
 Functions to easily build commonly used objects in inversions.
 """
 
-from .base import Combo
 from .conditions import ChiTarget
 from .directives import MultiplierCooler
 from .inversion import Inversion, InversionLogRich
-
-
-def create_standard_log(phi: Combo):
-    r"""
-    Define standard log for an inversion like :math:`\phi = \phi_d + \beta \phi_m`.
-    """
-    assert len(phi) == 2
-    data_misfit = phi[0]
-    regularization = phi[1]
-    assert hasattr(regularization, "multiplier")
-
-    columns = {
-        "iter": lambda iteration, _: iteration,
-        "beta": lambda _, __: regularization.multiplier,
-        "phi_d": lambda _, model: data_misfit(model),
-        "phi_m": lambda _, model: regularization.function(model),
-        "beta * phi_m": lambda _, model: regularization(model),
-        "phi": lambda _, model: phi(model),
-        "chi": lambda _, model: data_misfit(model) / data_misfit.n_data,
-    }
-    return InversionLogRich(columns)
 
 
 def create_inversion(
@@ -61,15 +39,7 @@ def create_inversion(
     stopping_criteria = ChiTarget(data_misfit, chi_target=chi_target)
 
     # Inversion log
-    columns = {
-        "iter": lambda iteration, _: iteration,
-        "beta": lambda _, __: regularization.multiplier,
-        "phi_d": lambda _, model: data_misfit(model),
-        "phi_m": lambda _, model: regularization.function(model),
-        "phi": lambda _, model: objective_function(model),
-        "chi": lambda _, model: data_misfit(model) / data_misfit.n_data,
-    }
-    inversion_log = InversionLogRich(columns)
+    inversion_log = InversionLogRich.create_from(objective_function)
 
     inversion = Inversion(
         objective_function,

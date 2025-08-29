5f67388329c8dc654070a72cd287526c05bbb77a
Santiago Soler <santisoler@fastmail.com>
2025-08-28 17:21:34 -0700

    Rename `optimizer` for `minimizer` in `Inversion` (#30)
    Rename the `optimizer` argument `minimizer` in `Inversion` and in the
    constructor function, to keep consistency with the `Minimizer` class.
    Add and improve docstring and type hints for `Inversion` and the
    constructor function.

--- src/inversion_ideas/constructors.py
+++ src/inversion_ideas/constructors.py
@@ -2,25 +2,59 @@
 Functions to easily build commonly used objects in inversions.
 """
 
+import numpy as np
+import numpy.typing as npt
+
+from .base import Minimizer, Objective
 from .conditions import ChiTarget
 from .directives import MultiplierCooler
 from .inversion import Inversion
 
 
 def create_inversion(
-    data_misfit,
-    model_norm,
+    data_misfit: Objective,
+    model_norm: Objective,
     *,
-    starting_beta,
-    initial_model,
-    optimizer,
-    beta_cooling_factor=2.0,
-    beta_cooling_rate=1,
-    chi_target=1.0,
-    cache_models=True,
+    starting_beta: float,
+    initial_model: npt.NDArray[np.float64],
+    minimizer: Minimizer,
+    beta_cooling_factor: float = 2.0,
+    beta_cooling_rate: int = 1,
+    chi_target: float = 1.0,
+    cache_models: bool = True,
 ) -> Inversion:
     r"""
     Create inversion of the form :math:`\phi_d + \beta \phi_m`.
+
+    Parameters
+    ----------
+    data_misfit : Objective
+        Data misfit term :math:`\phi_d`.
+    model_norm : Objective
+        Model norm :math:`\phi_m`.
+    starting_beta : float
+        Starting value for the trade-off parameter :math:`\beta`.
+    initial_model : npt.NDArray[np.float64]
+        Initial model to use in the inversion.
+    minimizer : Minimizer
+        Instance of :class:`Minimizer` used to minimize the objective function during
+        the inversion.
+    beta_cooling_factor : float, optional
+        Cooling factor for the trade-off parameter :math:`\beta`. Every
+        ``beta_cooling_rate`` iterations, the :math:`\beta` will be _cooled down_ by
+        dividing it by the ``beta_cooling_factor``.
+    beta_cooling_rate : int, optional
+        Cooling rate for the trade-off parameter :math:`\beta`. The trade-off parameter
+        will be cooled down every ``beta_cooling_rate`` iterations.
+    chi_target : float, optional
+        Target for the chi factor. The inversion will finish after the data misfit
+        reaches a :math:`\chi` factor lower or equal to ``chi_target``.
+    cache_models : bool, optional
+        Whether to cache models after each iteration in the inversion.
+
+    Returns
+    -------
+    Inversion
     """
     # Define objective function
     regularization = starting_beta * model_norm
@@ -41,7 +75,7 @@ def create_inversion(
     inversion = Inversion(
         objective_function,
         initial_model,
-        optimizer,
+        minimizer,
         directives=directives,
         stopping_criteria=stopping_criteria,
         cache_models=cache_models,

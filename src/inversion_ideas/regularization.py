a6f7f96fa77e4bead2a7b03bfe1bac6ec26264e8
Santiago Soler <santisoler@fastmail.com>
2025-09-26 15:15:14 -0700

    Improve weights on regularization and data misfit
    The weights in regularizations are now either a single array or
    a dictionary of arrays (similar to what is currently implemented in
    simpeg). Add a weights_matrix property to data misfit, so we can reuse
    it in the sensitivity weighting function.

--- src/inversion_ideas/regularization.py
+++ src/inversion_ideas/regularization.py
@@ -1,10 +1,12 @@
 """
 Regularization classes.
 """
-
+from copy import copy
+from collections.abc import Iterator
+from operator import mul
 import numpy as np
 import numpy.typing as npt
-from scipy.sparse import diags_array
+from scipy.sparse import dia_array, diags_array
 
 from .base import Objective
 
@@ -14,11 +16,18 @@ class TikhonovZero(Objective):
     Tikhonov zero-th order regularization.
     """
 
-    def __init__(self, n_params: int, weights=None, reference_model=None):
+    def __init__(
+        self,
+        n_params: int,
+        weights: npt.NDArray | dict[str, npt.NDArray] | None = None,
+        reference_model=None,
+    ):
         self._n_params = n_params
-        self.weights = (
-            weights if weights is not None else np.ones(n_params, dtype=np.float64)
-        )
+
+        if weights is None:
+            weights = np.ones(n_params, dtype=np.float64)
+        self.weights = weights
+
         self.reference_model = (
             reference_model
             if reference_model is not None
@@ -28,23 +37,23 @@ class TikhonovZero(Objective):
 
     def __call__(self, model) -> float:
         model_diff = model - self.reference_model
-        weights = diags_array(self.weights)
-        return model_diff.T @ weights.T @ weights @ model_diff
+        weights_matrix = self.weights_matrix
+        return model_diff.T @ weights_matrix.T @ weights_matrix @ model_diff
 
     def gradient(self, model):
         """
         Gradient vector.
         """
         model_diff = model - self.reference_model
-        weights = diags_array(self.weights)
-        return 2 * weights.T @ weights @ model_diff
+        weights_matrix = self.weights_matrix
+        return 2 * weights_matrix.T @ weights_matrix @ model_diff
 
     def hessian(self, model):  # noqa: ARG002
         """
         Hessian matrix.
         """
-        weights = diags_array(self.weights)
-        return 2 * weights.T @ weights
+        weights_matrix = self.weights_matrix
+        return 2 * weights_matrix.T @ weights_matrix
 
     def hessian_diagonal(self, model) -> npt.NDArray[np.float64]:
         """
@@ -58,3 +67,58 @@ class TikhonovZero(Objective):
         Number of model parameters.
         """
         return self._n_params
+
+    @property
+    def weights(self) -> npt.NDArray[np.float64] | dict[str, npt.NDArray[np.float64]]:
+        """
+        Regularization weights.
+        """
+        return self._weights
+
+    @weights.setter
+    def weights(
+        self, value: npt.NDArray[np.float64] | dict[str, npt.NDArray[np.float64]]
+    ):
+        """
+        Setter for weights.
+        """
+        if not isinstance(value, (np.ndarray, dict)):
+            msg = (
+                f"Invalid weights of type {type(value)}. "
+                "It must be an array or a dictionary."
+            )
+            raise TypeError(msg)
+        self._weights = value
+
+    @property
+    def weights_matrix(self) -> dia_array:
+        """
+        Diagonal matrix with the regularization weights.
+        """
+        if isinstance(self.weights, np.ndarray):
+            weights_array = self.weights
+        elif isinstance(self.weights, dict):
+            weights_array = _prod_arrays(iter(self.weights.values()))
+        else:
+            msg = f"Invalid weights of type '{type(self.weights)}'."
+            raise TypeError(msg)
+        return diags_array(weights_array)
+
+
+def _prod_arrays(arrays: Iterator[npt.NDArray[np.float64]]) -> npt.NDArray[np.float64]:
+    """
+    Compute product of arrays within an iterator.
+
+    Parameters
+    ----------
+    arrays : Iterator
+        Iterator with arrays.
+    """
+    if not arrays:
+        msg = "Invalid empty 'arrays' array when summing."
+        raise ValueError(msg)
+
+    result = copy(next(arrays))
+    for array in arrays:
+        result *= array
+    return result

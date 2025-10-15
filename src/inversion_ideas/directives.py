ddb111f8843268c2cb4c5a3ba68823fcd61b609e
Santiago Soler <santisoler@fastmail.com>
2025-10-15 14:31:29 -0700

    Autoformat

--- src/inversion_ideas/directives.py
+++ src/inversion_ideas/directives.py
@@ -7,8 +7,8 @@ import numpy.typing as npt
 
 from ._utils import extract_from_combo
 from .base import Combo, Directive, Objective, Scaled, Simulation
-from .utils import get_logger, get_sensitivity_weights
 from .typing import Model
+from .utils import get_logger, get_sensitivity_weights
 
 
 class MultiplierCooler(Directive):

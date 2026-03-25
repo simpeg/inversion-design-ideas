5489d4180d21d6af101f10671f5ca670354454b5
Santiago Soler <santisoler@fastmail.com>
2026-03-24 20:20:15 -0700

    Add a log for the minimizers (#59)
    Add `MinimizerLog` class that tracks information about minimizer calls.
    The minimizer logs are stored by the `Inversion` object itself. Improve
    the rich renders of the inversion log to include also the minimizer
    logs. Add a new `MinimizerResult` class that will be used by minimizers
    to pass as argument to callbacks. Add number of conjugate gradient
    iterations to `GaussNewtonConjugateGradient` that get passed to the
    `MinimizerResult`.

--- src/inversion_ideas/minimize/_minimizers.py
+++ src/inversion_ideas/minimize/_minimizers.py
@@ -9,10 +9,10 @@ from typing import Any
 import numpy as np
 from scipy.sparse.linalg import cg
 
-from ..base import Condition, Minimizer, Objective
+from ..base import Condition, Minimizer, MinimizerResult, Objective
 from ..errors import ConvergenceWarning
 from ..typing import Model, Preconditioner
-from ..utils import get_logger
+from ..utils import CountCalls, Counter, get_logger
 from ._utils import backtracking_line_search
 
 
@@ -62,16 +62,33 @@ class GaussNewtonConjugateGradient(Minimizer):
         self,
         objective: Objective,
         initial_model: Model,
-        preconditioner: Preconditioner
-        | Callable[[Model], Preconditioner]
-        | None = None,
+        *,
+        preconditioner: (
+            Preconditioner | Callable[[Model], Preconditioner] | None
+        ) = None,
+        callback: Callable[[MinimizerResult], None] | None = None,
     ) -> Generator[Model]:
         """
         Create iterator over Gauss-Newton minimization.
+
+        Parameters
+        ----------
+        objective : Objective
+            Objective function that will get minimized.
+        initial_model : (n_params) array
+            Initial model to start the minimization.
+        preconditioner : (n_params, n_params) array, sparray or LinearOperator or Callable, optional
+            Matrix used as preconditioner in the conjugant gradient algorithm.
+            If None, no preconditioner will be used.
+            A callable can be passed to build the preconditioner dynamically: such
+            callable should take a single ``initial_model`` argument and return an
+            array, `sparray` or a `LinearOperator`.
+        callback : callable, optional
+            Callable that gets called after each iteration.
         """
-        # Define a static preconditioner for all Gauss-Newton iterations
         cg_kwargs = self.cg_kwargs.copy()
 
+        # Define a static preconditioner for all Gauss-Newton iterations
         if preconditioner is not None:
             if "M" in self.cg_kwargs:
                 msg = "Cannot simultanously pass `preconditioner` and `M`."
@@ -84,9 +101,23 @@ class GaussNewtonConjugateGradient(Minimizer):
             cg_kwargs["M"] = preconditioner
 
         # Perform Gauss-Newton iterations
+        # -------------------------------
         iteration = 0
         phi_prev_value = np.inf  # value of the objective function on previous model
         model = initial_model.copy()
+
+        # Run callback before first yield
+        if callback is not None:
+            minimizer_result = MinimizerResult(
+                iteration=iteration,
+                model=model,
+                objective_value=objective(model),
+                cg_iters=None,
+                cg_converged=None,
+                line_search_iters=None,
+                step_norm=None,
+            )
+            callback(minimizer_result)
 
         # Yield initial model, so the generator is never empty
         yield model
@@ -95,7 +126,7 @@ class GaussNewtonConjugateGradient(Minimizer):
         while True:
             # Stop if reached max number of iterations
             if iteration >= self.maxiter:
-                get_logger().info(
+                get_logger().debug(
                     "⚠️ Reached maximum number of Gauss-Newton iterations "
                     f"({self.maxiter})."
                 )
@@ -113,19 +144,32 @@ class GaussNewtonConjugateGradient(Minimizer):
             if self.stopping_criteria is not None and self.stopping_criteria(model):
                 break
 
+            # Add a counter for conjugate-gradient iterations
+            if (key := "callback") not in cg_kwargs:
+                counter = Counter()
+                cg_kwargs[key] = counter
+            else:
+                # Decorate callback to count cg calls
+                cg_kwargs[key] = CountCalls(cg_kwargs[key])
+
             # Apply Conjugate Gradient to get search direction
             gradient, hessian = objective.gradient(model), objective.hessian(model)
-            search_direction, info = cg(hessian, -gradient, **cg_kwargs)
-            if info != 0:
+            search_direction, cg_code = cg(hessian, -gradient, **cg_kwargs)
+
+            # Get number of cg iterations from callback
+            cg_iters = cg_kwargs["callback"].counts
+            # Raise warning if cg didn't converge
+            cg_converged = cg_code == 0
+            if not cg_converged:
                 warnings.warn(
                     "Conjugate gradient convergence to tolerance not achieved after "
-                    f"{info} number of iterations.",
+                    f"{cg_code} number of iterations.",
                     ConvergenceWarning,
                     stacklevel=2,
                 )
 
             # Perform line search
-            alpha, n_ls_iters = backtracking_line_search(
+            alpha, line_search_iters = backtracking_line_search(
                 objective,
                 model,
                 search_direction,
@@ -136,16 +180,30 @@ class GaussNewtonConjugateGradient(Minimizer):
             if alpha is None:
                 msg = (
                     "Couldn't find a valid alpha, obtained None. "
-                    f"Ran {n_ls_iters} iterations."
+                    f"Ran {line_search_iters} iterations."
                 )
                 raise RuntimeError(msg)
 
             # Perform model step
-            model += alpha * search_direction
+            step = alpha * search_direction
+            model += step
 
             # Update cached values and iteration counter
             phi_prev_value = phi_value
             iteration += 1
 
-            # Yield inverted model for the current Gauss-Newon iteration
+            # Run callback before next yield
+            if callback is not None:
+                minimizer_result = MinimizerResult(
+                    iteration=iteration,
+                    model=model,
+                    objective_value=phi_value,
+                    cg_iters=cg_iters,
+                    cg_converged=cg_converged,
+                    line_search_iters=line_search_iters,
+                    step_norm=float(np.linalg.norm(step)),
+                )
+                callback(minimizer_result)
+
+            # Yield inverted model for the current Gauss-Newton iteration
             yield model
--- src/inversion_ideas/typing.py
+++ src/inversion_ideas/typing.py
@@ -2,12 +2,16 @@
 Custom types used for type hints.
 """
 
-from typing import Protocol, TypeAlias
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Protocol, TypeAlias
 
 import numpy as np
 import numpy.typing as npt
 from scipy.sparse import bsr_array, coo_array, csc_array, csr_array, dia_array
 from scipy.sparse.linalg import LinearOperator
+
+if TYPE_CHECKING:
+    from .base import MinimizerResult
 
 SparseArray: TypeAlias = bsr_array | coo_array | csc_array | csr_array | dia_array
 """
@@ -40,4 +44,16 @@ class SparseRegularization(Protocol):
         raise NotImplementedError
 
     def activate_irls(self, model_previous: Model) -> None:
+        raise NotImplementedError
+
+
+class Log(Protocol):
+    """
+    Protocol to define inversion and minimizer logs.
+    """
+
+    def update(self, iteration: int, model: Model) -> None:
+        raise NotImplementedError
+
+    def get_minimizer_callback(self) -> Callable[["MinimizerResult"], None]:
         raise NotImplementedError

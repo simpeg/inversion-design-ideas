fe39aeaaeef1dd5c42f60cdb3fc696f5dd888de6
Santiago Soler <santisoler@fastmail.com>
2026-04-10 11:22:33 -0700

    Update tests for DataMisfit
    Move functions to compare operators to `tests/utils.py`.

--- tests/base/test_objective_functions.py
+++ tests/base/test_objective_functions.py
@@ -7,10 +7,12 @@ import re
 
 import numpy as np
 import pytest
-from scipy.sparse import dia_array, sparray
+from scipy.sparse import dia_array
 from scipy.sparse.linalg import LinearOperator, aslinearoperator
 
 from inversion_ideas.base import Combo, Objective, Scaled
+
+from ..utils import assert_equal_linear_operators
 
 
 class Dummy(Objective):
@@ -81,46 +83,6 @@ class Dummy(Objective):
                 msg = f"Invalid hessian_type '{self.hessian_type}'."
                 raise ValueError(msg)
         return hessian
-
-
-def assert_equal_linear_operators(a, b, to_dense=False, seed=None, **kwargs):
-    """
-    Check if two linear operators are the same.
-
-    If ``a`` and ``b`` are ``LinearOperator``s, they will be compared by computing the
-    dot product with random arrays. Only the ``matvec`` and ``rmatvec`` will be tested.
-
-    Parameters
-    ----------
-    a, b : arrays, sparse arrays, or linear operators
-        Arrays or linear operators that will be tested.
-    to_dense : bool, optional
-        If True, sparse arrays will be converted to dense arrays for testing.
-        Use False for big matrices that can be too large to fit in memory.
-    seed : int or None, optional
-        Random seed used to define a random vector to test ``LinearOperator``s.
-        This argument will be ignored if ``a`` and ``b`` are not ``LinearOperator``s.
-    **kwargs : dict
-        Extra keyword arguments that will be passed to
-        :func:`numpy.testing.assert_equal`.
-    """
-    if to_dense:
-        if isinstance(a, sparray):
-            a = a.toarray()
-        if isinstance(b, sparray):
-            b = b.toarray()
-    if isinstance(a, np.ndarray) and isinstance(b, np.ndarray):
-        np.testing.assert_equal(a, b, **kwargs)
-    else:
-        assert a.dtype == b.dtype
-        assert a.shape == b.shape
-        # matvec
-        rng = np.random.default_rng(seed=seed)
-        vector = rng.uniform(size=a.shape[1])
-        np.testing.assert_equal(a @ vector, b @ vector, **kwargs)
-        # rmatvec
-        vector = rng.uniform(size=a.shape[0])
-        np.testing.assert_equal(a.T @ vector, b.T @ vector, **kwargs)
 
 
 class TestObjectiveOperations:
--- tests/test_data_misfit.py
+++ tests/test_data_misfit.py
@@ -3,10 +3,11 @@ Test the ``DataMisfit`` class.
 """
 
 import numpy as np
+import scipy.sparse as sp
 
 from inversion_ideas import DataMisfit
 
-from .utils import LinearRegressor
+from .utils import LinearRegressor, assert_allclose_linear_operators
 
 
 class TestDataMisfit:
@@ -17,9 +18,9 @@ class TestDataMisfit:
     n_params = 10
     n_data = 25
 
-    def test_hessian_diagonal(self):
+    def test_hessian_approx(self):
         """
-        Test the ``hessian_diagonal`` method.
+        Test the ``hessian_approx`` method.
         """
         # Generate some random data
         rng = np.random.default_rng(seed=42)
@@ -31,12 +32,16 @@ class TestDataMisfit:
         X = rng.uniform(size=self.n_data * self.n_params).reshape(shape)
         simulation = LinearRegressor(X)
 
-        # Define data misfit. Store full hessian for the test.
-        data_misfit = DataMisfit(data, uncertainties, simulation, build_hessian=True)
+        # Define data misfit
+        data_misfit = DataMisfit(data, uncertainties, simulation)
 
-        # Compare the true diagonal of the hessian with the one returned by
-        # hessian_diagonal.
+        # Get approximated hessian
         model = rng.uniform(size=self.n_params)
-        np.testing.assert_allclose(
-            data_misfit.hessian(model).diagonal(), data_misfit.hessian_diagonal(model)
-        )
+        hessian_approx = data_misfit.hessian_approx(model)
+
+        # Compare with expected one
+        full_hessian = DataMisfit(
+            data, uncertainties, simulation, build_hessian=True
+        ).hessian(model)
+        expected = sp.diags_array(full_hessian.diagonal())
+        assert_allclose_linear_operators(hessian_approx, expected, to_dense=True)

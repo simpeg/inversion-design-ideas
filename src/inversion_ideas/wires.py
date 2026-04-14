539b6db8be775331f3b356644e726a211ebb6825
Santiago Soler <santisoler@fastmail.com>
2026-04-14 12:43:30 -0700

    Extend docs for `Wires`

--- src/inversion_ideas/wires.py
+++ src/inversion_ideas/wires.py
@@ -21,6 +21,103 @@ class Wires:
 
     The ``Wires`` have capabilities for handling models with multiple physical
     properties.
+
+    Parameters
+    ----------
+    kwargs : dict
+        Keyword arguments. Each key represents the label of a portion of the model, and
+        its value should be an integer with the number of elements of that portion of
+        the model.
+
+    Examples
+    --------
+    Define a :class:`~inversion_ideas.Wires` object with two physical properties
+    (density and susceptibility), that hold different amount of model values for each.
+
+    >>> import numpy as np
+    >>> wires = Wires(density=3, susceptibility=4)
+    >>> wires.size
+    7
+    >>> wires.keys()
+    dict_keys(['density', 'susceptibility'])
+
+    Slice a model using the wires
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Define a model with 3 values for density and 4 for susceptibility:
+
+    >>> model = np.array([0.1, 0.2, -0.1, 1e-3, 1e-2, 1e-1, 0.3])
+
+    Slice the model to get the density values:
+
+    >>> wires.density.extract(model)
+    array([ 0.1,  0.2, -0.1])
+
+    Slice the model to get the susceptibility values:
+
+    >>> wires.susceptibility.extract(model)
+    array([0.001, 0.01 , 0.1  , 0.3  ])
+
+    Convert a model to a *model dictionary*
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    >>> wires.array_to_dict(model)
+    {'density': array([ 0.1,  0.2, -0.1]), 'susceptibility': array([0.001, 0.01 , 0.1  , 0.3  ])}
+
+
+    Define a :class:`~inversion_ideas.Wires` object from a model dictionary
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    >>> model_dict = {
+    ...     "conductivity": np.array([1e-4, 1e-2, 1e-1]),
+    ...     "density": np.array([-0.3, 0.15, 0.01]),
+    ... }
+    >>> wires = Wires.from_dict(model_dict)
+    >>> wires.keys()
+    dict_keys(['conductivity', 'density'])
+
+    We can use this ``Wires`` object to convert the model dictionary into an array:
+
+    >>> model = wires.dict_to_array(model_dict)
+    >>> model
+    array([ 1.0e-04,  1.0e-02,  1.0e-01, -3.0e-01,  1.5e-01,  1.0e-02])
+
+    Defining multiple model slices
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Let's say we have a model for the parameters of a sphere:
+
+    >>> model_sphere = {
+    ...     "radius": np.array([10.0]),
+    ...     "center": np.array([0.0, 0.0, -15.0]),
+    ...     "density": np.array([0.2]),
+    ...     "susceptibility": np.array([1e-2]),
+    ...     "conductivity": np.array([1e-3]),
+    ... }
+
+    We can create a ``Wires`` object from it:
+
+    >>> wires = Wires.from_dict(model_sphere)
+
+    We can access each individual ``ModelSlice`` as an attribute:
+
+    >>> wires.center  # doctest: +SKIP
+
+    Or by indexing it:
+
+    >>> wires["center"]  # doctest: +SKIP
+
+    And we can use these to extract pieces of the model array:
+
+    >>> model = wires.dict_to_array(model_sphere)
+    >>> wires["center"].extract(model)
+    array([  0.,   0., -15.])
+
+    We can also extract multiple slices:
+
+    >>> wires["radius", "center", "density"].extract(model)
+    array([ 10. ,   0. ,   0. , -15. ,   0.2])
+
     """
 
     def __init__(self, **kwargs):
@@ -72,9 +169,18 @@ class Wires:
         return self._slices[value]
 
     @classmethod
-    def create_from(cls, model_dict: dict[str, Model]):
+    def from_dict(cls, model_dict: dict[str, Model]):
         """
         Create a ``Wires`` object from a model dictionary.
+
+        Parameters
+        ----------
+        model_dict : dict
+            Dictionary with string labels and 1d arrays as values.
+
+        Returns
+        -------
+        Wires
         """
         kwargs = {key: array.size for key, array in model_dict.items()}
         return cls(**kwargs)
@@ -82,6 +188,18 @@ class Wires:
     def array_to_dict(self, model: Model):
         """
         Transform a model array into a dictionary.
+
+        Parameters
+        ----------
+        model : (n_params) array
+            Array with model values.
+
+        Returns
+        -------
+        dict
+            Dictionary with string labels and 1d arrays as values.
+            Each pair of key-value in the dictionary represents a portion of the model
+            array.
         """
         if model.size != self.size:
             # TODO: add msg
@@ -92,6 +210,16 @@ class Wires:
     def dict_to_array(self, model_dict: dict[str, Model]):
         """
         Ravel a model dictionary into a single 1D array.
+
+        Parameters
+        ----------
+        model_dict : dict
+            Dictionary with string labels and 1d arrays as values.
+
+        Returns
+        -------
+        array
+            1D array with model values.
         """
         # TODO: we should run sanity checks to ensure that the model_dict is compatible
         # with this wiring.

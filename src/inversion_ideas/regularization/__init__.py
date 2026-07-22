5f60d738895708dae89563d10fe96bddd769e2d3
Santiago Soler <santisoler@fastmail.com>
2026-07-22 10:35:30 -0700

    Add `TikhonovFirst` regularization

--- src/inversion_ideas/regularization/__init__.py
+++ src/inversion_ideas/regularization/__init__.py
@@ -2,12 +2,13 @@
 Regularization classes.
 """
 
-from ._general import TikhonovZero
+from ._general import TikhonovFirst, TikhonovZero
 from ._mesh_based import Flatness, Smallness, SparseSmallness
 
 __all__ = [
     "Flatness",
     "Smallness",
     "SparseSmallness",
+    "TikhonovFirst",
     "TikhonovZero",
 ]

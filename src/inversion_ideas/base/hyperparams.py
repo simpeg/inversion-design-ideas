b45be0e39c29d68e6682f1ee6acc73f21deac0b6
Santiago Soler <santisoler@fastmail.com>
2026-09-16 12:50:07 -0700

    Autoformat

--- src/inversion_ideas/base/hyperparams.py
+++ src/inversion_ideas/base/hyperparams.py
@@ -79,7 +79,7 @@ class Multiplier(Real):  # ruff: ignore[PLW1641] (ignore undefined __hash__ meth
         return f"{type(self).__name__}({self.value})"
 
     def _repr_latex_(self):
-        return r"$\text{" + f"{type(self).__name__}" + "}" f"({self.value})$"
+        return r"$\text{" + f"{type(self).__name__}" + f"}}({self.value})$"
 
     def __format__(self, fmt):
         return format(self.value, fmt)
